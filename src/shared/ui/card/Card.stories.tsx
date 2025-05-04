02afd7f4dfe8ee7ec092bb9786d158ba615bfd9c
eugenkin <evgensbk9@gmail.com>
2025-05-04 22:18:14 +0700

    SPR-13: fix all styles and import

--- src/shared/ui/card/Card.stories.tsx
+++ src/shared/ui/card/Card.stories.tsx
@@ -1,39 +1,44 @@
-import { Meta, StoryObj } from "@storybook/react"
-import { Card } from "./Card"
-import {Input} from '../Input/Input';
+import { Meta, StoryObj } from '@storybook/react'
+import { Card } from './Card'
+import { Input } from '../Input/Input'
+import { Button } from '../button/Button'
+import { Typography } from '../Typography/Typography'
 
 const meta: Meta<typeof Card> = {
-    title: 'Components/Card',
-    component: Card,
-    decorators: [(story) => <div style={{ margin: '3rem' }}>{story()}</div>],
-    tags:["autodocs"],
-    
-  } satisfies Meta<typeof Card>;
-  
-  export default meta
-  
-  type Story = StoryObj<typeof Card>
+  title: 'Components/Card',
+  component: Card,
 
-  export const CardWithEmailInput: Story = {
-    render: () => (
-      <Card title="Вход">
-        <Input 
+  tags: ['autodocs'],
+} satisfies Meta<typeof Card>
+
+export default meta
+
+type Story = StoryObj<typeof Card>
+
+export const CardEmpty: Story = {}
+
+export const CardWithAuth: Story = {
+  render: () => (
+    <Card
+      title={
+        <Typography as="label" variant="boldText14">
+          Sign In
+        </Typography>
+      }
+      footer={
+        <>
+          <Button variant="primary">Primary Button</Button>
+          <Button variant="secondary">Secondary Button</Button>
+        </>
+      }
+    >
+      {/* <Input 
           label="Email" 
           variant="email" 
           placeholder="Введите email" 
-        />
-      </Card>
-    ),
-  };    
-
-  export const CardWithPasswordInput: Story = {
-    render: () => (
-      <Card title="Регистрация">
-        <Input
-          label="Пароль" 
-          variant="password" 
-          placeholder="Введите пароль" 
-        />
-      </Card>
-    ),
-  };
+        /> */}
+      <input placeholder="Email" />
+      <input placeholder="Password" />
+    </Card>
+  ),
+}
--- src/shared/ui/card/Card.tsx
+++ src/shared/ui/card/Card.tsx
@@ -1,29 +1,23 @@
-import { forwardRef, HTMLAttributes, ReactNode } from 'react'
-import clsx from 'classnames'
+import { ReactNode } from 'react'
 import styles from './Card.module.scss'
 
-
 export interface CardProps {
-    title?: string;
-    children: ReactNode;
-    footer?: ReactNode;
-    className?: string;
+  title?: ReactNode
+  children: ReactNode
+  footer?: ReactNode
+  className?: string
 }
 
 export const Card: React.FC<CardProps> = ({ title, children, footer }) => {
-   
-
-    return (
-        <div className={styles.cardWrapper}>
-            <div className={styles.cardField}>
-            {title && <h3 className="font-bold text-lg mb-2">{title}</h3>}
-            <div className={styles.cardContent}>{children}</div>
-            {footer && <div className="border-t pt-2">{footer}</div>}
-            </div>   
-        </div>
-    );
-};
+  return (
+    <div className={styles.cardWrapper}>
+      <div className={styles.cardField}>
+        {title && <div className={styles.cardHeader}>{title}</div>}
+        <div className={styles.cardContent}>{children}</div>
+        {footer && <div className={styles.cardFooter}>{footer}</div>}
+      </div>
+    </div>
+  )
+}
 
 Card.displayName = 'Card'
-
-

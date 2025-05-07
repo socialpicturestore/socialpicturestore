9dd5d962510df2cf1bc43f4c84b17d2c22a0c2f8
matveev96 <sasha.matveev1996@icloud.com>
2025-05-07 23:31:26 +0300

    SPR-20 feat: realize DatePicker with 3 mode and calendar icons

--- src/shared/ui/DatePicker/DatePicker.tsx
+++ src/shared/ui/DatePicker/DatePicker.tsx
@@ -1,174 +1,122 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useId, useRef, useState } from 'react'
 import 'react-day-picker/style.css'
 import clsx from 'classnames'
 import { type DateRange, DayPicker } from 'react-day-picker'
 import styles from './DatePicker.module.scss'
 import { enGB } from 'date-fns/locale'
-import { format } from 'date-fns'
+import { format, isValid, parse } from 'date-fns'
 import { Calendar, CalendarOutline } from '@/shared/assets/icons'
 
 export type DatePickerProps = {
-  variant: 'range' | 'multiple'
+  mode: 'range' | 'multiple' | 'single'
   disabled?: boolean
 }
 
 const isSingleDayRange = (range?: DateRange) =>
   range?.from && range?.to && range.from.getTime() === range.to.getTime()
-// const isSingleDayRange = (range?: DateRange) =>
-//   range?.from && range?.to && range.from.getTime() === range.to.getTime()
 
 export const DatePicker = ({ mode, disabled }: DatePickerProps) => {
-  // const [range, setRange] = useState<DateRange | undefined>()
-  // const [multiple, setMultiple] = useState<Date[] | undefined>()
-  // const [month, setMonth] = useState(new Date())
-  // // const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined)
-  // const [inputValue, setInputValue] = useState('')
-  // const [isDialogOpen, setIsDialogOpen] = useState(false)
-  // const wrapperRef = useRef<HTMLDivElement>(null)
-  //
-  // useEffect(() => {
-  //   const handleClickOutside = (e: MouseEvent) => {
-  //     if (wrapperRef.current && !wrapperRef.current.contains(e.target as Node)) {
-  //       setIsDialogOpen(false)
-  //     }
-  //   }
-  //   document.addEventListener('mousedown', handleClickOutside)
-  //   return () => document.removeEventListener('mousedown', handleClickOutside)
-  // }, [])
-  //
-  // const isSingle = isSingleDayRange(range)
-  //
-  // const selected =
-  //   variant === 'range' ? (isSingle ? { from: range?.from, to: undefined } : range) : multiple
-  //
-  // const handleSelect = (value: any) => {
-  //   if (variant === 'range') setRange(value as DateRange)
-  //   else setMultiple(value as Date[])
-  // }
-  //
-  // const displayValue =
-  //   range?.from && range?.to
-  //     ? `${format(range.from, 'dd/MM/yyyy')} - ${format(range.to, 'dd/MM/yyyy')}`
-  //     : range?.from
-  //       ? format(range.from, 'dd/MM/yyyy')
-  //       : ''
-  //
-  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setInputValue(e.target.value)
-  //
-  //   const parsedDate = parse(e.target.value, 'dd/MM/yyyy', new Date())
-  //
-  //   if (isValid(parsedDate)) {
-  //     // setSelectedDate(parsedDate)
-  //     setMonth(parsedDate)
-  //   } else {
-  //     // setSelectedDate(undefined)
-  //   }
-  // }
-  //
-  // return (
-  //   <div ref={wrapperRef} onFocus={() => setIsDialogOpen(true)}>
-  //     <input
-  //       placeholder={'dd/MM/yyyy'}
-  //       className={styles.inputBox}
-  //       onChange={handleInputChange}
-  //       value={inputValue}
-  //       disabled={disabled}
-  //     >
-  //       <span>{displayValue || 'Select data'}</span>
-  //       {isDialogOpen ? <Calendar /> : <CalendarOutline />}
-  //     </input>
-  //     {isDialogOpen && (
-  //       <div className={styles.calendarWrapper}>
-  //         <DayPicker
-  //           month={month}
-  //           onMonthChange={setMonth}
-  //           locale={enGB}
-  //           animate
-  //           showOutsideDays
-  //           mode={variant}
-  //           selected={selected}
-  //           onSelect={handleSelect}
-  //           modifiers={{
-  //             weekend: date => date.getDay() === 0 || date.getDay() === 6,
-  //           }}
-  //           modifiersClassNames={{
-  //             weekend: styles.myWeekend,
-  //             outside: styles.myOutside,
-  //             selected: clsx({
-  //               [styles.mySelected]: isSingle,
-  //             }),
-  //           }}
-  //           className={styles.customDaypicker}
-  //           classNames={{
-  //             range_start: clsx({
-  //               [styles.myRangeStart]: !isSingle,
-  //             }),
-  //             range_end: clsx({
-  //               [styles.myRangeEnd]: !isSingle,
-  //             }),
-  //             range_middle: styles.myRangeMiddle,
-  //             today: styles.myToday,
-  //             button_previous: styles.myNavBtn,
-  //             button_next: styles.myNavBtn,
-  //             caption_label: styles.myCaptionLabel,
-  //             selected: styles.myMultipleSelected,
-  //           }}
-  //         />
-  //       </div>
-  //     )}
-  //   </div>
-  // )
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const dialogId = useId()
+  const headerId = useId()
 
-export const DatePicker = ({ variant, disabled }: DatePickerProps) => {
-  const [range, setRange] = useState<DateRange | undefined>()
-  const [multiple, setMultiple] = useState<Date[] | undefined>()
-  const [open, setOpen] = useState(false)
+  const [month, setMonth] = useState(new Date())
+  const [selectedSingle, setSelectedSingle] = useState<Date | undefined>()
+  const [selectedMultiple, setSelectedMultiple] = useState<Date[] | undefined>([])
+  const [selectedRange, setSelectedRange] = useState<DateRange | undefined>()
+  const [inputValue, setInputValue] = useState('')
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const isSingle = isSingleDayRange(selectedRange)
+  const selected =
+    mode === 'single' ? selectedSingle : mode === 'multiple' ? selectedMultiple : selectedRange
 
   useEffect(() => {
-    const handleClickOutside = (e: MouseEvent) => {
+    const handleOutsideClick = (e: MouseEvent) => {
       if (wrapperRef.current && !wrapperRef.current.contains(e.target as Node)) {
-        setOpen(false)
+        setIsDialogOpen(false)
       }
     }
-    document.addEventListener('mousedown', handleClickOutside)
-    return () => document.removeEventListener('mousedown', handleClickOutside)
-  }, [])
 
-  const isSingle = isSingleDayRange(range)
-  const selected =
-    variant === 'range' ? (isSingle ? { from: range?.from, to: undefined } : range) : multiple
-  const handleSelect = (value: any) => {
-    if (variant === 'range') setRange(value as DateRange)
-    else setMultiple(value as Date[])
+    if (isDialogOpen) {
+      document.addEventListener('mousedown', handleOutsideClick)
+    } else {
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  }, [isDialogOpen])
+
+  const handleSelect = (value: Date | Date[] | DateRange | undefined) => {
+    if (mode === 'single' && value instanceof Date) {
+      setSelectedSingle(value)
+      setInputValue(format(value, 'dd/MM/yyyy'))
+      setIsDialogOpen(false)
+    }
+
+    if (mode === 'multiple' && Array.isArray(value)) {
+      setSelectedMultiple(value)
+      setInputValue(value.map(d => format(d, 'dd/MM/yyyy')).join(', '))
+    }
+
+    if (mode === 'range' && value && typeof value === 'object') {
+      setSelectedRange(value as DateRange)
+      const { from, to } = value as DateRange
+      if (from && to) {
+        setInputValue(`${format(from, 'dd/MM/yyyy')} – ${format(to, 'dd/MM/yyyy')}`)
+      } else if (from) {
+        setInputValue(format(from, 'dd/MM/yyyy'))
+      }
+    }
   }
-  const wrapperRef = useRef<HTMLDivElement>(null)
-  const displayValue =
-    range?.from && range?.to
-      ? `${format(range.from, 'dd/MM/yyyy')} - ${format(range.to, 'dd/MM/yyyy')}`
-      : range?.from
-        ? format(range.from, 'dd/MM/yyyy')
-        : ''
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+    const parsedDate = parse(e.target.value, 'dd/MM/yyyy', new Date())
+    if (!isValid(parsedDate)) return
+    setMonth(parsedDate)
+    if (mode === 'single') setSelectedSingle(parsedDate)
+    if (mode === 'multiple') setSelectedMultiple([parsedDate])
+    if (mode === 'range') setSelectedRange({ from: parsedDate, to: undefined })
+  }
 
   return (
     <div ref={wrapperRef}>
-      <button
-        className={styles.inputBox}
-        onClick={() => setOpen(prev => !prev)}
-        disabled={disabled}
-      >
-        <span>{displayValue || 'Select data'}</span>
-        {open ? <Calendar /> : <CalendarOutline />}
-      </button>
-      {open && (
-        <div className={styles.calendarWrapper}>
+      <div className={styles.inputWrapper}>
+        <input
+          className={clsx(styles.inputBox, { [styles.inputBoxOpen]: isDialogOpen })}
+          id="date-input"
+          type="text"
+          value={inputValue}
+          placeholder="Select date"
+          onChange={handleInputChange}
+          onFocus={() => setIsDialogOpen(true)}
+          disabled={disabled}
+        />
+        <span className={styles.calendarIcon}>
+          {isDialogOpen ? (
+            <Calendar color={'#ffffff'} width={20} height={20} />
+          ) : (
+            <CalendarOutline color={'#ffffff'} width={20} height={20} />
+          )}
+        </span>
+      </div>
+      {isDialogOpen && (
+        <div
+          role="dialog"
+          id={dialogId}
+          aria-labelledby={headerId}
+          className={styles.calendarWrapper}
+        >
           <DayPicker
             locale={enGB}
-            animate
-            showOutsideDays
-            mode={variant}
+            mode={mode}
             selected={selected}
             onSelect={handleSelect}
+            month={month}
+            onMonthChange={setMonth}
+            showOutsideDays
             modifiers={{
               weekend: date => date.getDay() === 0 || date.getDay() === 6,
             }}

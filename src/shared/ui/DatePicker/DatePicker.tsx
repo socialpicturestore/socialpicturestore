01405a72d6a0fba8ea3ec0402baf0814aa113ba8
matveev96 <sasha.matveev1996@icloud.com>
2025-05-13 12:48:30 +0300

    SPR-20 feat: complete DayPicker

--- src/shared/ui/DatePicker/DatePicker.tsx
+++ src/shared/ui/DatePicker/DatePicker.tsx
@@ -4,31 +4,30 @@ import clsx from 'classnames'
 import { type DateRange, DayPicker } from 'react-day-picker'
 import styles from './DatePicker.module.scss'
 import { enGB } from 'date-fns/locale'
-import { format, isValid, parse } from 'date-fns'
+import { endOfWeek, format, startOfWeek } from 'date-fns'
 import { Calendar, CalendarOutline } from '@/shared/assets/icons'
 
 export type DatePickerProps = {
-  mode: 'range' | 'multiple' | 'single'
+  mode: 'range' | 'single' | 'week'
   disabled?: boolean
+  setSelectedDate: (value: Date | DateRange | undefined) => void
+  selectedDate: Date | DateRange | undefined
+  error?: string
 }
 
-const isSingleDayRange = (range?: DateRange) =>
-  range?.from && range?.to && range.from.getTime() === range.to.getTime()
-
-export const DatePicker = ({ mode, disabled }: DatePickerProps) => {
+export const DatePicker = ({
+  mode,
+  disabled,
+  setSelectedDate,
+  selectedDate,
+  error,
+}: DatePickerProps) => {
   const wrapperRef = useRef<HTMLDivElement>(null)
   const dialogId = useId()
   const headerId = useId()
 
-  const [selectedSingle, setSelectedSingle] = useState<Date | undefined>()
-  const [selectedMultiple, setSelectedMultiple] = useState<Date[] | undefined>([])
-  const [selectedRange, setSelectedRange] = useState<DateRange | undefined>()
   const [inputValue, setInputValue] = useState('')
   const [isDialogOpen, setIsDialogOpen] = useState(false)
-  const isSingle = isSingleDayRange(selectedRange)
-  const [error, setError] = useState('')
-  const selected =
-    mode === 'single' ? selectedSingle : mode === 'multiple' ? selectedMultiple : selectedRange
 
   useEffect(() => {
     const handleOutsideClick = (e: MouseEvent) => {
@@ -49,47 +48,23 @@ export const DatePicker = ({ mode, disabled }: DatePickerProps) => {
 
   const handleSelect = (value: Date | Date[] | DateRange | undefined) => {
     if (mode === 'single' && value instanceof Date) {
-      setSelectedSingle(value)
+      setSelectedDate(value)
       setInputValue(format(value, 'dd/MM/yyyy'))
       setIsDialogOpen(false)
     }
 
-    if (mode === 'multiple' && Array.isArray(value)) {
-      setSelectedMultiple(value)
-      setInputValue(value.map(d => format(d, 'dd/MM/yyyy')).join(', '))
-    }
-
-    if (mode === 'range' && value && typeof value === 'object') {
-      setSelectedRange(value as DateRange)
+    if (mode === 'range') {
       const { from, to } = value as DateRange
-      if (from && to) {
-        setInputValue(`${format(from, 'dd/MM/yyyy')} – ${format(to, 'dd/MM/yyyy')}`)
-      } else if (from) {
-        setInputValue(format(from, 'dd/MM/yyyy'))
+      if (from?.getDay() === to?.getDay()) {
+        setSelectedDate({ from, to: undefined })
+        setInputValue(`${format(from || 'dd/MM/yyyy', 'dd/MM/yyyy')} – dd/MM/yyyy`)
+      } else {
+        setSelectedDate(value as DateRange)
+        setInputValue(
+          `${format(from || 'dd/MM/yyyy', 'dd/MM/yyyy')} – ${format(to || 'dd/MM/yyyy', 'dd/MM/yyyy')}`
+        )
       }
     }
-  }
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
-
-    setInputValue(value)
-
-    const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/
-    if (!dateRegex.test(value)) {
-      setError('Error! Enter date in format dd/MM/yyyy')
-      return
-    }
-    const parsedDate = parse(value, 'dd/MM/yyyy', new Date())
-    if (!isValid(parsedDate)) {
-      return
-    }
-
-    if (mode === 'single') setSelectedSingle(parsedDate)
-    if (mode === 'multiple') setSelectedMultiple([parsedDate])
-    if (mode === 'range') setSelectedRange({ from: parsedDate, to: undefined })
-
-    setError('')
   }
 
   return (
@@ -98,7 +73,7 @@ export const DatePicker = ({ mode, disabled }: DatePickerProps) => {
         <input
           className={clsx(styles.inputBox, {
             [styles.inputBoxOpen]: isDialogOpen,
-            [styles.inputBoxError]: !!error,
+            [styles.inputBoxError]: error,
           })}
           id="date-input"
           type="text"
@@ -106,11 +81,10 @@ export const DatePicker = ({ mode, disabled }: DatePickerProps) => {
           placeholder={
             mode === 'single'
               ? 'Select date'
-              : mode === 'multiple'
-                ? 'Select multiple date'
-                : 'Select range date'
+              : mode === 'range'
+                ? 'Select range date'
+                : 'Select week date'
           }
-          onChange={handleInputChange}
           disabled={disabled}
         />
         <span className={styles.calendarIcon}>
@@ -130,35 +104,55 @@ export const DatePicker = ({ mode, disabled }: DatePickerProps) => {
           className={styles.calendarWrapper}
         >
           <DayPicker
-            locale={enGB}
-            mode={mode}
-            selected={selected}
-            onSelect={handleSelect}
             showOutsideDays
+            locale={enGB}
+            mode={mode === 'week' ? 'single' : mode}
+            selected={selectedDate}
+            onSelect={handleSelect}
             modifiers={{
               weekend: date => date.getDay() === 0 || date.getDay() === 6,
+              hoverable: date => {
+                if (mode === 'single' && selectedDate instanceof Date) {
+                  return format(date, 'yyyy-MM-dd') !== format(selectedDate, 'yyyy-MM-dd')
+                }
+                if (mode !== 'single' && selectedDate && typeof selectedDate === 'object') {
+                  const { from, to } = selectedDate as DateRange
+                  const isInRange =
+                    (from && format(date, 'yyyy-MM-dd') === format(from, 'yyyy-MM-dd')) ||
+                    (to && format(date, 'yyyy-MM-dd') === format(to, 'yyyy-MM-dd')) ||
+                    (from && to && date > from && date < to)
+                  return !isInRange
+                }
+
+                return true
+              },
+            }}
+            onDayClick={day => {
+              if (mode === 'week') {
+                setInputValue(
+                  `${format(startOfWeek(day, { weekStartsOn: 1 }), 'dd/MM/yyyy')} – ${format(endOfWeek(day, { weekStartsOn: 1 }), 'dd/MM/yyyy')}`
+                )
+                setSelectedDate({
+                  from: startOfWeek(day, { weekStartsOn: 1 }),
+                  to: endOfWeek(day, { weekStartsOn: 1 }),
+                })
+              }
             }}
             modifiersClassNames={{
               weekend: styles.myWeekend,
               outside: styles.myOutside,
-              selected: clsx({
-                [styles.mySelected]: isSingle,
-              }),
+              selected: styles.mySelected,
+              hoverable: styles.myHover,
             }}
             className={styles.customDaypicker}
             classNames={{
-              range_start: clsx({
-                [styles.myRangeStart]: !isSingle,
-              }),
-              range_end: clsx({
-                [styles.myRangeEnd]: !isSingle,
-              }),
+              range_start: styles.myRangeStart,
+              range_end: styles.myRangeEnd,
               range_middle: styles.myRangeMiddle,
               today: styles.myToday,
               button_previous: styles.myNavBtn,
               button_next: styles.myNavBtn,
               caption_label: styles.myCaptionLabel,
-              selected: styles.myMultipleSelected,
             }}
           />
         </div>

2056bc84bfabad016682410943d678644c227105
Vika <vika.degtyareva.12@mail.ru>
2025-05-23 16:37:20 +0300

    SPR-rtk-query chore: include Header in StoreProvider scope

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${interSans.variable} antialiased`}>
-        <Header />
-        <StoreProvider>{children}</StoreProvider>
+        <StoreProvider>
+          <Header />
+          {children}
+        </StoreProvider>
       </body>
     </html>
   )

6f7e8dfd6b3080dbbc5b2437a7ffe8fc14b4a659
matveev96 <sasha.matveev1996@icloud.com>
2025-06-04 16:01:18 +0300

    feat: add className to blocks for styling

--- src/widgets/forgot-password-section/ForgotPassword.tsx
+++ src/widgets/forgot-password-section/ForgotPassword.tsx
@@ -41,28 +41,32 @@ export const ForgotPassword = () => {
   return (
     <Card>
       <form className={s.container} onSubmit={handleSubmit(onSubmit)}>
-        <Typography variant={'h1'}>Forgot Password</Typography>
-
-        <Input
-          {...register('email')}
-          error={errors.email?.message}
-          label={'Email'}
-          placeholder={'example@example.com'}
-        ></Input>
-
+        <Typography className={s.forgotTitle} variant={'h1'}>
+          Forgot Password
+        </Typography>
+        <div className={s.inputEmail}>
+          <Input
+            {...register('email')}
+            error={errors.email?.message}
+            label={'Email'}
+            placeholder={'example@example.com'}
+          ></Input>
+        </div>
         <Typography className={s.paragraph} variant={'regularText14'}>
           Enter your email address and we will send you further instructions{' '}
         </Typography>
 
         {isSuccess && (
-          <Typography className={s.paragraph} variant={'regularText14'}>
+          <Typography variant={'regularText14'}>
             The link has been sent by email. If you don’t receive an email send link again
           </Typography>
         )}
 
-        <Button type="submit">Send Link</Button>
+        <Button type="submit" className={s.sendButton}>
+          Send Link
+        </Button>
 
-        <Button asChild variant={'text'}>
+        <Button asChild variant={'text'} className={s.backButton}>
           <Link href={'/'}>Back to Sign In</Link>
         </Button>
 

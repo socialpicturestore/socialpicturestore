a5ce7f44b5f1671130256e36de273fe9b4486f3e
matveev96 <sasha.matveev1996@icloud.com>
2025-06-08 14:42:42 +0300

    refactor: remove zod and move files to anothe folder

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ForgotPassword } from '@/widgets/forgot-password-section'
+import { ForgotPassword } from '@/widgets/(password-recovery)/forgot-password-section'
 
 const page = () => {
   return <ForgotPassword />
--- src/shared/lib/validation/schemas.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod'
-
-export const forgotPasswordSchema = z.object({
-  email: z.string().email('Введите корректный email'),
-  recaptcha: z.string().nonempty('Подтвердите, что вы не робот'),
-})

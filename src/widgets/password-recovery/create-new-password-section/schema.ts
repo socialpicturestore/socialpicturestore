c3cbe15f85467f5f8e911ba292a2f2214b32b1f1
matveev96 <sasha.matveev1996@icloud.com>
2025-06-15 15:07:26 +0300

    refactor: change max number of symbols in passwordSchema

--- src/widgets/password-recovery/create-new-password-section/schema.ts
+++ src/widgets/password-recovery/create-new-password-section/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 const passwordSchema = z
   .string()
   .min(6, { message: 'Minimum number of characters 6' })
-  .max(30, { message: 'Maximum number of characters 30' })
+  .max(20, { message: 'Maximum number of characters 20' })
   .refine(value => /[0-9]/.test(value), {
     message: 'Password must contain at least one number (0-9)',
   })
